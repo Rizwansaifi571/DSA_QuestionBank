38386f06705002ecdefb4715800bcb79d52c6768
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-01-07 21:00:19 +0530

    Array I

--- Day-1/CountElementsWithMaximumFrequency.cpp
+++ Day-1/CountElementsWithMaximumFrequency.cpp
@@ -35,10 +35,51 @@ Constraints:
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int maxFrequencyElements(vector<int>& nums) {
+    sort(nums.begin(), nums.end()); // Sort the array
+    
+    int maxFreq = 0;
+    int currentFreq = 1;
+    int count = 0;
 
+    // Traverse the sorted array
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] == nums[i - 1]) {
+            currentFreq++; // If the current element is the same as the previous one, increment the frequency
+        } else {
+            if (currentFreq == maxFreq) {
+                count += currentFreq; // Add the frequency to the total count
+            }
+            if (currentFreq > maxFreq) {
+                maxFreq = currentFreq; // Update max frequency
+                count = currentFreq; // Reset count to the current frequency
+            }
+            currentFreq = 1; // Reset frequency for the new element
+        }
+    }
+    
+    // Check for the last element group
+    if (currentFreq == maxFreq) {
+        count += currentFreq;
+    } else if (currentFreq > maxFreq) {
+        count = currentFreq;
+    }
 
-int main () {
+    return count;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 2, 3, 1, 4};
+    vector<int> nums2 = {1, 2, 3, 4, 5};
+    vector<int> nums1 = {10, 12, 11, 9, 6, 19, 11};
+    
+    cout << maxFrequencyElements(nums) << endl;  // Output: 4
+    cout << maxFrequencyElements(nums1) << endl; // Output: 2
+    cout << maxFrequencyElements(nums2) << endl; // Output: 5
+    
     return 0;
 }
