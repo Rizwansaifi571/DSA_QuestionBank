26a02f061e8d416d56c06c8495af1140ea6889be
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-01-12 23:33:10 +0530

    Array II

---
+++ Dat-003/bubbleSort.cpp
@@ -0,0 +1,30 @@
+// bubble sort
+
+#include <iostream>
+using namespace std;
+
+void bubblesort(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i; j++) {
+            if(arr[j + 1] < arr[j]) {
+                swap(arr[j + 1], arr[j]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " " ;
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    bubblesort(arr, 5);
+    printArray(arr, 5);
+    return 0;
+
+
+}
