565efea9626cd79bc2c29d9e1841db1b8898ffdf
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-01-28 10:32:19 +0530

    LetCode Question Streak

--- LeetCode_Streak/Day-004/Majority_Element.cpp
+++ LeetCode_Streak/Day-004/Majority_Element.cpp
@@ -27,3 +27,28 @@ n == nums.length
 
 */
 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int majority = nums[0];
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(majority == nums[i]) {
+                count++;
+            }
+            else {
+                count--;
+            }
+
+            if(count <= 0) {
+                majority = nums[i];
+                count++;
+            }
+        }
+        return majority;
+    }
