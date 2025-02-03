a36d50007683495263fc54c18db27f7c8e42525e
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-02-03 10:48:18 +0530

    LetCode Question Streak

--- LeetCode_Streak/Day-006/Product_of_Array_Except_Self.cpp
+++ LeetCode_Streak/Day-006/Product_of_Array_Except_Self.cpp
@@ -28,3 +28,24 @@ Constraints:
 The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n, 1);
+
+        int prefix = 1;
+        for(int i = 0; i < n; i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return answer;
+    }
