6aee0b4f7a2c95c5c3bc3f08c01550b9ea12877f
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-02-22 11:57:48 +0530

    LetCode Question Streak

--- LeetCode_Streak/Day-012/Isomorphic_Strings.cpp
+++ LeetCode_Streak/Day-012/Isomorphic_Strings.cpp
@@ -49,3 +49,21 @@ s and t consist of any valid ascii character.
 
 */
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()) return false;
+        vector<int> s_map(256, -1);
+        vector<int> t_map(256, -1);
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s_map[s[i]] != t_map[t[i]]) {
+                return false;
+            }
+            s_map[s[i]] = t_map[t[i]] = i;
+        }
+        return true;
+    }
