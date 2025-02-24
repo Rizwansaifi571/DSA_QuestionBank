a28ede400d1091f2386e454e107c1f8549b76dd8
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-02-24 12:09:27 +0530

    LetCode Question Streak

--- LeetCode_Streak/Day-014/Multiply_Strings.cpp
+++ LeetCode_Streak/Day-014/Multiply_Strings.cpp
@@ -27,3 +27,30 @@ Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 
 */
 
+#include <iostream> 
+#include <vector>
+#include <string>
+using namespace std;    
+
+string multiply(string num1, string num2) {
+        int n = num1.length();
+        int m = num2.length();
+        vector<int> result(n + m, 0);
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                int index1 = num1[i] - '0';
+                int index2 = num2[j] - '0';
+                int sum = result[i + j + 1] + index1 * index2;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        string product;
+        for(int num : result) {
+            num = num + '0';
+            if(!(product.empty() && num == '0'))
+                product.push_back(num);
+        }
+        return product.empty() ? "0" : product;
