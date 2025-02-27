635020e61cca3e600b9cd402a4b69019a5f353d4
rizwansaifi571 <rizwansaifi2614@gmail.com>
2025-02-27 12:33:43 +0530

    LetCode Question Streak

---
+++ LeetCode_Streak/Day-017/License_Key_Formatting.cpp
@@ -0,0 +1,71 @@
+/*
+482. License Key Formatting
+<Easy>
+https://leetcode.com/problems/license-key-formatting/description/
+
+You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+
+We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+
+Return the reformatted license key.
+
+ 
+
+Example 1:
+
+Input: s = "5F3Z-2e-9-w", k = 4
+Output: "5F3Z-2E9W"
+Explanation: The string s has been split into two parts, each part has 4 characters.
+Note that the two extra dashes are not needed and can be removed.
+Example 2:
+
+Input: s = "2-5g-3-J", k = 2
+Output: "2-5G-3J"
+Explanation: The string s has been split into three parts, each part has 2 characters except the first part as it could be shorter as mentioned above.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of English letters, digits, and dashes '-'.
+1 <= k <= 104
+
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string upgrade(string s) {
+        string ans = "";
+        for(char c : s) {
+            if(c >= 'a' && c <= 'z') {
+                c = c - 'a' + 'A';
+                ans.push_back(c);
+            }
+            else if(c != '-') {
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+
+    string licenseKeyFormatting(string s, int k) {
+        string ans = upgrade(s);
+        string result = "";
+        int count = 0;
+
+        for(int i = ans.length() - 1; i >= 0; i--) {
+            result.push_back(ans[i]);
+            count++;
+            if(count == k) {
+                result.push_back('-');
+                count = 0;
+            }
+        }
+
+        if(!result.empty() && result.back() == '-') result.pop_back();
+        
+        reverse(result.begin(), result.end());
+        return result;
